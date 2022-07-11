e25fc52a9d4d6a434dc49b22a9b314e8b5c06721
Damon <damonspriggle@hotmail.com>
2022-07-10 19:15:12 -0500

    draft without walkthorugh

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
+const mongoose = require('mongoose')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,11 +9,13 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+  useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
+// Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
